7cc13d09cd2a4fa335b2a4ca7a68db4378335033
carloosbravo <carloosbravo04@gmail.com>
2023-11-04 18:31:50 +0100

    ArrayList with participantes created

--- juegoCaramelosThreads/src/main/Main.java
+++ juegoCaramelosThreads/src/main/Main.java
@@ -1,7 +1,24 @@
 package main;
 
+import java.util.ArrayList;
+
+import objects.Cuenco;
+import objects.Participante;
+
 public class Main {
     public static void main(String[] args){
         
+        ArrayList <Participante> participantes = new ArrayList<Participante>();
+        // Cuenco cuenco = new Cuenco();
+        int numeroParticipantes = 10;
+
+        for(int i = 0;i<numeroParticipantes;i++){
+            Participante p = new Participante();
+            participantes.add(p);
+            
+
+
+        }
+
     }
 }
