5099df3b9c609c8f9f8e04ce5a5498d11370b5a8
carloosbravo <carloosbravo04@gmail.com>
2023-11-04 19:06:19 +0100

    GettersSetters created

--- juegoCaramelosThreads/src/objects/Participante.java
+++ juegoCaramelosThreads/src/objects/Participante.java
@@ -1,18 +1,44 @@
 package objects;
 
 public class Participante extends Thread {
-    
+
     private int id;
     private int cestaCaramelos;
     private Cuenco cuenco;
 
-    public Participante( int id, Cuenco cuenco){
+    public Participante(int id, Cuenco cuenco) {
         this.id = id;
         this.cuenco = cuenco;
 
     }
 
-    public  void run(){
-            
+    public void run() {
+
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCestaCaramelos() {
+        return cestaCaramelos;
+    }
+
+    public void setCestaCaramelos(int cestaCaramelos) {
+        this.cestaCaramelos = cestaCaramelos;
+    }
+
+    public Cuenco getCuenco() {
+        return cuenco;
+    }
+
+    public void setCuenco(Cuenco cuenco) {
+        this.cuenco = cuenco;
+    }
+
+    
 }
